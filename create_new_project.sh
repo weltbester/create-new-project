aa6abe966118b18828d002e405f706caad314e8e
Georg Pohl <georgpohl@gmx.de>
2024-12-15 17:57:06 +0100

    Add common branches to script. Renamed 'master' branch in script.

--- create_new_project.sh
+++ create_new_project.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 # This script creates a new project folder, depending on the programming language in use.
 
+# Folder structure:
+# .
+# ├── c
+# ├── cpp
+# ├── create_new_project
+# ├── gitvorlagen
+# ├── py
+# ├── rust
+# ├── sh
+# └── skripte
+
+# gitvorlagen
+# ├── c_template
+# ├── cpp_template
+# ├── python-vorlage
+# ├── rust_template
+# └── shell_template.sh
+
 # Get projet data from user
 echo "This script creates a new programming project."
 read -p "Enter new project name (snake_case_style): " project_name
-read -p "Enter programming language suffix, e.g.: c, cpp, sh, py: " programming_suffix
+read -p "Enter programming language suffix, e.g.: c, cpp, sh, py, rs: " programming_suffix
 
 # Delete folder if already existent
 if [ -d "$programming_suffix/$project_name" ]; then
@@ -27,6 +45,10 @@ if [[ "$programming_suffix" == "py" ]]; then
 elif [[ "$programming_suffix" == "sh" ]]; then 
     cp ../../gitvorlagen/shell_template.sh .
     mv shell_template.sh $filename
+    printf "Rust"
+elif [[ "$programming_suffix" == "rs" ]]; then 
+    cp ../../gitvorlagen/rust_template/* .
+    mv src/main.rs $filename
     printf "Shell"
 elif [[ "$programming_suffix" == "c" ]]; then 
     cp -r ../../gitvorlagen/c_template/* .
@@ -47,16 +69,25 @@ printf "Initialize git for repository '${project_name}'\n\n"
 pwd
 git init
 
-if [[ "$programming_suffix" == "py" || "$programming_suffix" == "sh"  ]]; then
+if [[ "$programming_suffix" == "py" || "$programming_suffix" == "sh" ||
+      "programming_suffix" == "rs" ]]; then
     echo "# $filename" > README.md
 fi
 
 git add .
 git commit -m "Initial commit"
-# git remote add origin https://gitlab.devops.telekom.de/georg.pohl/$filename.git
-# git remote add origin https://gitlab.devops.telekom.de/ip-core/$filename.git
-# git push -u origin main
+git branch -M main
+
+git branch bugfix
+git branch feature
+git branch develop
+git branch release
+git branch hotfix
 git status
+
+# git remote add origin git@github.com/$filename.git
+# git remote add origin https://github.com/$filename.git
+# git push -u origin main
 
 # Startup Emacs
 # printf "\n\nOpen '${filename}' in emacs\n\n" 
